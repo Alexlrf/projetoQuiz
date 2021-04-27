9addb814357be516775f7567e9e3295f69828de6
Alexlrf <alexrfogazzi@gmail.com>
2021-04-26 21:40:37 -0300

    Ação no botão da TelaLogin para alterar cor ao passar o mouse; tool tip text no jtextField

--- src/main/java/com/projeto/view/TelaLogin.java
+++ src/main/java/com/projeto/view/TelaLogin.java
@@ -1,6 +1,7 @@
 package com.projeto.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -9,6 +10,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TelaLogin extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -39,20 +42,37 @@ public class TelaLogin extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setForeground(Color.WHITE);
+		contentPane.setBackground(Color.DARK_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Usu\u00E1rio");
+		lblNewLabel.setForeground(Color.WHITE);
 		lblNewLabel.setBounds(94, 95, 46, 14);
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
+		textField.setToolTipText("Digite o usuário cadastrado");
 		textField.setBounds(164, 92, 185, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Entrar");
+		btnNewButton.setBackground(Color.WHITE);
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnNewButton.setBackground(Color.black);
+				btnNewButton.setForeground(Color.white);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnNewButton.setBackground(Color.WHITE);
+				btnNewButton.setForeground(Color.black);
+			}
+		});
 		btnNewButton.setBounds(260, 152, 89, 23);
 		contentPane.add(btnNewButton);
 	}
