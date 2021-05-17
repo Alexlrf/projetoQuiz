af04e21c7294d1ca9e5fbb1176ab89f29bdbf54f
alison.vizoni <alison.vizoni@FLE207D18.alunos.sc.senac.br>
2021-05-17 09:44:44 -0300

    Teste em aula...

--- src/main/java/com/projeto/enums/UsuarioEnum.java
+++ src/main/java/com/projeto/enums/UsuarioEnum.java
@@ -13,6 +13,7 @@ public enum UsuarioEnum {
 	public static UsuarioEnum getEstagioPesquisa(String usuarioEnum){
         for(UsuarioEnum usuario:UsuarioEnum.values()){
             if(usuario.toString().equals(usuarioEnum.toUpperCase())){
+            	eu vou zuar xom esta classe ♦♦A♦1↔☻♣♦•52♣♣74
                 return usuario;
             }
         }
