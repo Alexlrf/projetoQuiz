a98ebea2ac2f0c26789788fc832c3f72555abe64
Rodrigo Lobato <rodlobatobass@gmail.com>
2021-05-18 22:15:05 -0300

    Atualização PerguntaVO.

--- src/main/java/com/projeto/model/entity/PerguntaVO.java
+++ src/main/java/com/projeto/model/entity/PerguntaVO.java
@@ -1,10 +1,18 @@
 package com.projeto.model.entity;
 
-public class PerguntaVO {
+import java.io.Serializable;
+
+public class PerguntaVO implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Long idPergunta;
 	private String texto;
 	private String categoria;
+	
+	private AlternativaVO questao;
+	
 	
 	public PerguntaVO(Long idPergunta, String texto, String categoria) {
 		super();
@@ -39,6 +47,46 @@ public class PerguntaVO {
 
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
-	}	
+	}
 
+	public AlternativaVO getQuestao() {
+		return questao;
+	}
+
+	public void setQuestao(AlternativaVO questao) {
+		this.questao = questao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idPergunta == null) ? 0 : idPergunta.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PerguntaVO other = (PerguntaVO) obj;
+		if (idPergunta == null) {
+			if (other.idPergunta != null)
+				return false;
+		} else if (!idPergunta.equals(other.idPergunta))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "idPergunta=" + idPergunta
+				+ "\ntexto=" + texto
+				+ "\ncategoria=" + categoria;
+	}	
+    
 }
