310f8d4fe2a66400787bb0be5f9aa15ac713b7de
Rodrigo Lobato <rodlobatobass@gmail.com>
2021-05-18 22:44:48 -0300

    Criação da classe PerguntaDAO e auteração na exception PerguntaNaoExistenteException.

--- src/main/java/com/projeto/exceptions/PerguntaNaoExistenteException.java
+++ src/main/java/com/projeto/exceptions/PerguntaNaoExistenteException.java
@@ -1,9 +1,24 @@
 package com.projeto.exceptions;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class PerguntaNaoExistenteException extends Exception {
 
 	public PerguntaNaoExistenteException(String msg) {
 		super(msg);
+	}
+
+
+	public static void closeStatement(PreparedStatement st) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public static void closeResultSet(ResultSet rs) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
