ad9500e641dfdc651fce6185076c16fc88752499
alison.vizoni <alison.vizoni@FLE207D18.alunos.sc.senac.br>
2021-05-24 09:59:50 -0300

    Remoção de imports não usados.

--- src/main/java/com/projeto/view/TelaMenuCoordenador.java
+++ src/main/java/com/projeto/view/TelaMenuCoordenador.java
@@ -1,25 +1,22 @@
 package com.projeto.view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JPasswordField;
-import javax.swing.JButton;
-import javax.swing.JRadioButton;
 import java.awt.Font;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import net.miginfocom.swing.MigLayout;
 
 public class TelaMenuCoordenador extends JFrame {
 
