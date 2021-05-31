e35401a67370855a50843badd6d38f67df27fbe3
ALison Manarin <alison.vizoni@gmail.com>
2021-05-31 11:19:58 -0300

    Desenvolver relatório de usuario com seletores

---
+++ src/main/java/com/projeto/view/PanelRelatorioDeUsuario.java
@@ -0,0 +1,14 @@
+package com.projeto.view;
+
+import javax.swing.JPanel;
+
+public class PanelRelatorioDeUsuario extends JPanel {
+
+	/** TIPO DE USUARIO, NOME COM LIKE, PESQUISAR TODOS. LEMBRAR DE ACRESCENTAR PAGINAÇÃO e relatório excel.
+	 * Create the panel.
+	 */
+	public PanelRelatorioDeUsuario() {
+
+	}
+
+}
